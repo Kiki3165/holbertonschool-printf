30ba295c8aa44028de379d61afa4a6fe7f1ae64e
MoeAlAnnan <5653@holbertonstudents.com>
2022-11-09 08:02:21 -0800

    header file

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef
-#define
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,14 +7,19 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-
+int _printf(const char * const format, ...);
 void print_char(va_list ap);
 void print_string(va_list ap);
 void print_percent(va_list ap);
-
+/**
+ * struct print - a structure
+ *
+ * @t_arg: The operator associated with a function
+ * @f: The function associated
+ */
 typedef struct print
 {
-	char *t_arg;
+	char t_arg;
 	void (*f)(va_list);
 } print_arg;
 
