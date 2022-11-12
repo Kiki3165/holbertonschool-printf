ed7c9b32e2c8941b72a3e59211091dc25f1160ea
kiki3165 <kikikou82@gmail.com>
2022-11-12 15:08:01 +0000

    printf_function

---
+++ _printf.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _printf - function to print
+ * @format: string
+ * Return: counter
+ */
+
+int _printf(const char *format, ...)
+{
+	int counter;
+	va_list flag;
+
+	delim del[] = {
+		{"c", cfunc},
+		{"s", sfunc},
+		{"d", dfunc},
+		{"i", dfunc},
+		{0, NULL}
+	};
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(flag, format);
+	counter = get_del(format, del, flag);
+	va_end(flag);
+
+	return (counter);
+}
