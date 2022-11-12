fa9a7173add85c2247772b0d20dd64dde0907949
Kiki3165 <113670617+Kiki3165@users.noreply.github.com>
2022-11-12 16:55:22 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+
+int _printf(const char * const format, ...);
+int print_char(va_list ap);
+int print_str(va_list ap);
+int print_integer(va_list ap);
+int get_ops(const char *format, print_arg ops[], va_list ap);
+
+/**
+ * struct print - a structure
+ *
+ * @t_arg: The operator associated with a function
+ * @f: The function associated
+ */
+typedef struct print
+{
+	char t_arg;
+	int (*f)(va_list);
+} print_arg;
+
+#endif
